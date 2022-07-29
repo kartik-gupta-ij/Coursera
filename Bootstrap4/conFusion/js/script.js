0f6720e3061575013e3e267cc315283204603693
kartik-gupta-ij <kartikgupta7267@gmail.com>
2022-07-29 17:23:26 +0530

    fixed part 1

---
+++ Bootstrap4/conFusion/js/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    $("#mycarousel").carousel({ interval: 2000 });
+    $("#carouselButton").click(function () {
+        if ($("#carouselButton").children("span").hasClass('fa-play')) {
+            $("#mycarousel").carousel('cycle');
+            $("#carouselButton").children("span").removeClass('fa-play');
+            $("#carouselButton").children("span").addClass('fa-pause');
+        }
+        else if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+            $("#mycarousel").carousel('pause');
+            $("#carouselButton").children("span").removeClass('fa-pause');
+            $("#carouselButton").children("span").addClass('fa-play');
+        }
+
+    });
+    $("#TableBtn").click(function () {
+        $('#Table').modal('show')
+    });
+    $("#LoginBtn").click(function () {
+        $('#loginModal').modal('show')
+    });
+
+});
