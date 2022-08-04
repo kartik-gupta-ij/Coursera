15503f00067caea55186a08d3961015e02155ac2
kartik-gupta-ij <kartikgupta7267@gmail.com>
2022-08-04 10:46:32 +0530

    era

--- React/confusion/src/App.js
+++ React/confusion/src/App.js
@@ -1,26 +1,12 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Menu from './components/MenuComponent';
-import {DISHES} from './shared/dishes.js';
-
-import './App.css';
+import Main from './components/MainComponent';
 
 class App extends Component {
-  constructor(probs){
-    super(probs);
-    this.state={
-      dishes:DISHES
-    };
-  }
+
   render() {
     return (
-      <div>
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
-        <Menu dishes={this.state.dishes} />
+      <div className="App">
+        <Main />
       </div>
     );
   }
--- React/confusion/src/components/MenuComponent.js
+++ React/confusion/src/components/MenuComponent.js
@@ -19,7 +19,8 @@ class Menu extends Component {
         const menu = this.props.dishes.map((dish) => {
             return (
               <div className="col-12 col-md-5 m-1">
-                <Card key={dish.id} onClick={()=>this.onDishSelect(dish)}>
+                <Card key={dish.id}
+                        onClick={() => this.props.onClick(dish.id)}>
                   
                   <CardImg width="100%" object src={dish.image} alt={dish.name} />
                   
