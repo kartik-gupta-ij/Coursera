e0313619e0e6b3ada4e2a68ac6634808ea8faa15
kartik-gupta-ij <kartikgupta7267@gmail.com>
2022-08-04 13:38:41 +0530

    Header and Footer

--- React/confusion/src/components/DishdetailComponent.js
+++ React/confusion/src/components/DishdetailComponent.js
@@ -1,54 +1,55 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {Card,CardImg,CardBody,CardText,CardTitle} from 'reactstrap';
 
 
-
-
-class DishdetailComponent extends Component {
-
-
-    renderDish(dish){
-        if(dish!=null){
-          return(
-            <div className='row'>
-            <div className='col-12 col-md-5 m-1'>
-          <Card key={dish.id} >
-                    
-          <CardImg top object src={dish.image} alt={dish.name} />
-          
-          <CardBody >
-            <CardTitle>{dish.name}</CardTitle> 
-            <CardText>{dish.description}</CardText>
-          </CardBody>
-        </Card>
-        </div>
-        <div className='col-12 col-md-5 m-1'>
-            <h4>Comments</h4>
-        {this.props.dish.comments.map((commentp)=>{
-        return(
-            <div key={commentp.id}>
-            <h5>{commentp.comment}</h5>
-            <p>--{commentp.author}, {new Intl.DateTimeFormat('en-US', {year: 'numeric', month: 'long', day: '2-digit'}).format(new Date(commentp.date))}</p>
-            </div>
-        )
-
-    })
-    }    
-        </div>
-        </div>)
-        }else{
-          return (<div></div>)
-        }
-      }
-    render(){
-
-    return(
-            <div>
-            {this.renderDish(this.props.dish)}
-            </div>
-     )
-    }
-    
+function RenderDish({dish}) {
+  return(
+    <div className='col-12 col-md-5 m-1'>
+      <Card key={dish.id} >
+        <CardImg top object src={dish.image} alt={dish.name} />
+        <CardBody >
+          <CardTitle>{dish.name}</CardTitle> 
+          <CardText>{dish.description}</CardText>
+        </CardBody>
+      </Card>
+    </div>
+  );
 }
 
-export default DishdetailComponent;
+function RenderComments({comments}) {
+  const Comments =comments.map((commentp)=> {
+    return(
+      <div key={commentp.id}>
+          <h5>{commentp.comment}</h5>
+          <p>--{commentp.author}, {new Intl.DateTimeFormat('en-US', {year: 'numeric', month: 'long', day: '2-digit'}).format(new Date(commentp.date))}</p>
+      </div>
+    );
+
+  });
+  return(
+    <div className='col-12 col-md-5 m-1'>
+      <h4>Comments</h4>
+        {Comments}
+    </div>
+  );
+}
+
+const  DishDetail = (props) => {
+  if(props.dish!=null){
+      return(
+
+        <div className='container'>
+          <div className='row'>
+            <RenderDish dish={props.dish}/>
+            <RenderComments comments={props.dish.comments}/>
+          </div>
+        </div>
+    )
+  }
+  else{
+    return (<div></div>)
+  }
+}
+
+
+export default DishDetail;
--- React/confusion/src/components/MainComponent.js
+++ React/confusion/src/components/MainComponent.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
 import Menu from './MenuComponent';
 import DishDetail from './DishdetailComponent';
 import { DISHES } from '../shared/dishes';
@@ -20,15 +22,12 @@ class Main extends Component {
 
   render() {
     return (
-      <div>
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
+      <React.Fragment>
+        <Header />
         <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
         <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
-      </div>
+        <Footer />
+      </React.Fragment>
     );
   }
 }
