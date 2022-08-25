455ff0e7f3cb11126380beb6cad3ae8abba950f8
kartik-gupta-ij <kartikgupta7267@gmail.com>
2022-08-25 23:03:01 +0530

    Redux Thunk

--- React/confusion/src/components/DishdetailComponent.js
+++ React/confusion/src/components/DishdetailComponent.js
@@ -3,6 +3,7 @@ import { Card, CardImg, CardText, CardBody,
   CardTitle, Breadcrumb, BreadcrumbItem, Modal, ModalHeader, ModalBody,Button, Label ,Col,Row} from 'reactstrap';
   import { Control,LocalForm,Errors } from 'react-redux-form'
 import { Link } from 'react-router-dom';
+import { Loading } from './LoadingComponent';
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => (val) && (val.length <= len)
 const minLength = (len) => (val) => (val) && (val.length >= len)
@@ -138,8 +139,25 @@ class CommentFormComponent extends Component{
     }
 
     const DishDetail = (props) => {
-        if (props.dish != null)
-
+      if (props.isLoading) {
+        return(
+            <div className="container">
+                <div className="row">            
+                    <Loading />
+                </div>
+            </div>
+        );
+    }
+    else if (props.errMess) {
+        return(
+            <div className="container">
+                <div className="row">            
+                    <h4>{props.errMess}</h4>
+                </div>
+            </div>
+        );
+    }
+    else if (props.dish != null) 
         {
         return(
           <div className="container">
--- React/confusion/src/components/MenuComponent.js
+++ React/confusion/src/components/MenuComponent.js
@@ -2,21 +2,44 @@ import React from 'react';
 import { Card, CardImg, CardImgOverlay,
     CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { Loading } from './LoadingComponent';
 
-    function RenderMenuItem ({dish, onClick}) {
-        return (
-            <Card>
-                <Link to={`/menu/${dish.id}`} >
-                    <CardImg width="100%" src={dish.image} alt={dish.name} />
-                    <CardImgOverlay>
-                        <CardTitle>{dish.name}</CardTitle>
-                    </CardImgOverlay>
-                </Link>
-            </Card>
-        );
+function RenderMenuItem ({dish, onClick}) {
+
+            return (
+                <Card>
+                    <Link to={`/menu/${dish.id}`} >
+                        <CardImg width="100%" src={dish.image} alt={dish.name} />
+                        <CardImgOverlay>
+                            <CardTitle>{dish.name}</CardTitle>
+                        </CardImgOverlay>
+                    </Link>
+                </Card>
+            );
     }
     const Menu=(props)=>{
-        const menu = props.dishes.map((dish) => {
+        if (props.dishes.isLoading) {
+            return(
+                <div className="container">
+                    <div className="row">            
+                        <Loading />
+                    </div>
+                </div>
+            );
+        }
+        else if (props.dishes.errMess) {
+            return(
+                <div className="container">
+                    <div className="row"> 
+                        <div className="col-12">
+                            <h4>{props.dishes.errMess}</h4>
+                        </div>
+                    </div>
+                </div>
+            );
+        }
+        else{
+        const menu = props.dishes.dishes.map((dish) => {
           return (
             <div className="col-12 col-md-5 m-1"  key={dish.id}>
                 <RenderMenuItem dish={dish} onClick={props.onClick} />
@@ -42,6 +65,7 @@ import { Link } from 'react-router-dom';
             </div>
         );
     }
+}
 
 
 export default Menu;
