a36c23abaa13202342d30d12bf5cf348fbc7afd5
Kartik Gupta <kartikgupta7267@gmail.com>
2022-10-18 08:23:47 +0530

    Express Generator REST API

---
+++ NodeJS/conFusionServer/routes/promoRouter.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const promotionsRouter = express.Router();
+
+promotionsRouter.use(bodyParser.json());
+
+promotionsRouter.route('/')
+.all((req,res,next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req,res,next) => {
+    res.end('Will send all the promotionss to you!');
+})
+.post((req, res, next) => {
+    res.end('Will add the promotions: ' + req.body.name + ' with details: ' + req.body.description);
+})
+.put((req, res, next) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /promotions');
+})
+.delete((req, res, next) => {
+    res.end('Deleting all promotions');
+});
+
+promotionsRouter.route('/:promoId')
+.all((req,res,next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req,res,next) => {
+    res.end('Will send details of the Promotion: ' + req.params.promoId +' to you!');
+})
+.post((req, res, next) => {
+  res.statusCode = 403;
+  res.end('POST operation not supported on /promotionss/'+ req.params.promoId);
+})
+.put( (req, res, next) => {
+  res.write('Updating the Promotions: ' + req.params.promoId + '\n');
+  res.end('Will update the Promotions: ' + req.body.name + 
+        ' with details: ' + req.body.description);
+})
+.delete((req, res, next) => {
+    res.end('Deleting Promotions: ' + req.params.promoId);
+});
+module.exports = promotionsRouter;
