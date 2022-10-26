102dcdc354a65f3fe36bdf3487caa126cd5b7a14
Kartik Gupta <kartikgupta7267@gmail.com>
2022-10-26 15:57:34 +0530

    Mongoose Part 2

--- NodeJS/node-mongoose/index.js
+++ NodeJS/node-mongoose/index.js
@@ -13,23 +13,25 @@ connect.then((db) => {
         name: 'Uthappizza',
         description: 'test'
     });
+    Dishes.create({
+      name: 'Uthapizza',
+      description: 'Test'
+  })
+  .then((dish) => {
+      console.log(dish);
+      
+      return Dishes.find({}).exec();
+  })
+  .then((dishes) => {
+      console.log(dishes);
 
-    newDish.save()
-        .then((dish) => {
-            console.log(dish);
-
-            return Dishes.find({});
-        })
-        .then((dishes) => {
-            console.log(dishes);
-
-            return Dishes.remove({});
-        })
-        .then(() => {
-            return mongoose.connection.close();
-        })
-        .catch((err) => {
-            console.log(err);
-        });
+      return Dishes.remove({});
+  })
+  .then(() => {
+      return mongoose.connection.close();
+  })
+  .catch((err) => {
+      console.log(err);
+  });
 
 });
