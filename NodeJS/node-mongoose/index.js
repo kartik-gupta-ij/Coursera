ebe3efe074587ba6d99abd8ae96591d8ed70b140
Kartik Gupta <kartikgupta7267@gmail.com>
2022-10-26 16:00:22 +0530

    Mongoose Part 3

--- NodeJS/node-mongoose/index.js
+++ NodeJS/node-mongoose/index.js
@@ -14,16 +14,32 @@ connect.then((db) => {
         description: 'test'
     });
     Dishes.create({
-      name: 'Uthapizza',
-      description: 'Test'
+      name: 'Uthappizza',
+      description: 'test'
   })
   .then((dish) => {
       console.log(dish);
-      
-      return Dishes.find({}).exec();
+
+      return Dishes.findByIdAndUpdate(dish._id, {
+          $set: { description: 'Updated test'}
+      },{ 
+          new: true 
+      })
+      .exec();
   })
-  .then((dishes) => {
-      console.log(dishes);
+  .then((dish) => {
+      console.log(dish);
+
+      dish.comments.push({
+          rating: 5,
+          comment: 'I\'m getting a sinking feeling!',
+          author: 'Leonardo di Carpaccio'
+      });
+
+      return dish.save();
+  })
+  .then((dish) => {
+      console.log(dish);
 
       return Dishes.remove({});
   })
@@ -33,5 +49,4 @@ connect.then((db) => {
   .catch((err) => {
       console.log(err);
   });
-
 });
--- NodeJS/node-mongoose/models/dishes.js
+++ NodeJS/node-mongoose/models/dishes.js
@@ -1,18 +1,38 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const dishSchema = new Schema({
-    name: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    description: {
-        type: String,
-        required: true
-    }
-},{
-    timestamps: true
+var commentSchema = new Schema({
+  rating:  {
+      type: Number,
+      min: 1,
+      max: 5,
+      required: true
+  },
+  comment:  {
+      type: String,
+      required: true
+  },
+  author:  {
+      type: String,
+      required: true
+  }
+}, {
+  timestamps: true
+});
+
+var dishSchema = new Schema({
+  name: {
+      type: String,
+      required: true,
+      unique: true
+  },
+  description: {
+      type: String,
+      required: true
+  },
+  comments:[commentSchema]
+}, {
+  timestamps: true
 });
 
 var Dishes = mongoose.model('Dish', dishSchema);
