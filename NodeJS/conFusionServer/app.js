d8a6063ffc028b1dfec6182ff604dd6ccbd7cc0e
Kartik Gupta <kartikgupta7267@gmail.com>
2022-11-17 00:50:55 +0530

    File Upload

--- NodeJS/conFusionServer/app.js
+++ NodeJS/conFusionServer/app.js
@@ -14,6 +14,7 @@ var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
+const uploadRouter = require('./routes/uploadRouter');
 
 var app = express();
 
@@ -46,6 +47,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dishes',dishRouter);
 app.use('/promotions',promoRouter);
 app.use('/leaders',leaderRouter);
+app.use('/imageUpload',uploadRouter);
 
 const mongoose = require('mongoose');
 
