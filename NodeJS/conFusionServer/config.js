f9a1cafeff262b9dae5bca180507df9129f62228
Kartik Gupta <kartikgupta7267@gmail.com>
2022-11-17 17:56:25 +0530

    Passport Facebook

--- NodeJS/conFusionServer/config.js
+++ NodeJS/conFusionServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
   'secretKey': '12345-67890-09876-54321',
-  'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+  'mongoUrl': 'mongodb://localhost:27017/conFusion',
+  'facebook': {
+      clientId: '1117579315794244',
+      clientSecret: '243128fd1489d149372754f90d0dc61c'
+  }
 }
